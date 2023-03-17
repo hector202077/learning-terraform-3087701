5a712313d15db272b135438c2dec3a50064f6796
hector202077 <71443855+hector202077@users.noreply.github.com>
2023-03-17 08:49:44 -0700

    VPC V2b

--- main.tf
+++ main.tf
@@ -35,10 +35,10 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg[0]]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
