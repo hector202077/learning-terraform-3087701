f8404dd4dffeb93a9a1d124c430231a11989dfca
hector202077 <71443855+hector202077@users.noreply.github.com>
2023-03-17 09:48:08 -0700

    Autoscale V1

--- main.tf
+++ main.tf
@@ -31,18 +31,35 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+#Instance code but now being created by blog_alb module
+#resource "aws_instance" "blog" {
+#  ami           = data.aws_ami.app_ami.id
+#  instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+#  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+#  tags = {
+#    Name = "Learning Terraform"
+#  }
+#}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  # insert the 1 required variable here
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type 
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
